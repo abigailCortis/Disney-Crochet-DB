f7dfe344478242d1dd48bc1e93044a5c5dadb688
Abigail Cortis <cortisbiggirl@gmail.com>
2024-08-15 11:56:09 +0200

    added data for issue 6

---
+++ sql/V3__issues6-10.sql
@@ -0,0 +1,15 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(6, 'Mickey and Minnie Yellow Flower', ARRAY['Vanilla', 'Sunflower'], 1)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(6, 'Toy Story E-Reader Case', 1),
+(6, 'Pretty Tangled Purse', 4),
+(6, 'Nemo Ombre Cushion', 2)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(6, 'Double Treble'),
+(6, 'Triple Treble'),
+(6, 'Working into a Space')
+;
