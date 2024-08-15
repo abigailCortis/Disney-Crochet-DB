f4abb4bf1bfab3ffd3558ba10de674bc84563b8f
Abigail Cortis <cortisbiggirl@gmail.com>
2024-08-15 14:12:06 +0200

    added data for issue 7

--- sql/V3__issues6-10.sql
+++ sql/V3__issues6-10.sql
@@ -1,15 +1,23 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
-(6, 'Mickey and Minnie Yellow Flower', ARRAY['Vanilla', 'Sunflower'], 1)
+(6, 'Mickey and Minnie Yellow Flower', ARRAY['Vanilla', 'Sunflower'], 1),
+(7, 'Winnie the Pooh Square', ARRAY['Bright Red', 'White', 'Sunflower', 'Black'], 1),
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (6, 'Toy Story E-Reader Case', 1),
 (6, 'Pretty Tangled Purse', 4),
-(6, 'Nemo Ombre Cushion', 2)
+(6, 'Nemo Ombre Cushion', 2),
+(7, 'Coco Cowl', 1),
+(7, 'Princess Aurora Shopper', 2),
+(7, 'Frozen Place Mats' 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (6, 'Double Treble'),
 (6, 'Triple Treble'),
-(6, 'Working into a Space')
+(6, 'Working into a Space'),
+(7, 'Shell Edging'),
+(7, 'The Magic Ring'),
+(7, 'Turning Chains'),
+(7, 'Joining a New Yarn')
 ;
