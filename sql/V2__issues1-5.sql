b8d98ee3438e53323e77886da40f94e30d2dfc2d
Abigail Cortis <cortisbiggirl@gmail.com>
2024-08-15 11:51:31 +0200

    added data for issues 1-5

---
+++ sql/V2__issues1-5.sql
@@ -0,0 +1,50 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(1, 'Mickey Motif', ARRAY['Sky Blue', 'Coral'], 1),
+(2, 'Moana Garland', ARRAY['Cyan', 'Hibiscus', 'White'], 1),
+(3, 'Jungle Book Classic Granny Square', ARRAY['Lime', 'White', 'Coral'], 1),
+(4, 'Winnie the Pooh Flower & Bees Square', ARRAY['Sage', 'Sunflower', 'Black'], 2),
+(5, 'Tangled Rose Bouquet', ARRAY['Candy Pink', 'Shell Pink', 'Lime'], 1)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(1, 'Jungle Book Plant Hangers', 2),
+(1, 'Ariel Baby Blanket', 3),
+(1, 'Mickey Bunting', 2),
+(2, 'Minnie Headband', 2),
+(2, 'Jasmine Shawl', 3),
+(2, 'Thumper Storage Basket', 3),
+(3, 'Disney Princess Headbands', 1),
+(3, 'Finding Dory Key Ring', 3),
+(3, 'Winnie th Pooh Coasters', 1),
+(4, 'Frozen Snowflake Motif Scarf', 2),
+(4, 'Toy Story Alien Hat', 4),
+(4, 'Dumbo Circus Cushion', 2),
+(5, 'Gorgeous Princess Flower Clips', 2),
+(5, 'Daisy Duck Sleep Mask', 2),
+(5, 'Jessie & Woody Cushions', 4)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(1, 'Double Crochet'),
+(1, 'Tension'),
+(1, 'Slip Knot'),
+(1, 'Chain Stitch'),
+(1, 'Slip Stitch'),
+(1, 'Slip Stitch - Foundation Ring'),
+(1, 'Slip Stitch - Finishing Stitch'),
+(2, 'Half Treble'),
+(2, 'Blocking your finished item'),
+(2, 'How to Hold your Hook'),
+(2, 'Fastening Off'),
+(2, 'Hook Size'),
+(3, 'Treble Crochet'),
+(3, 'Amigurumi Basics'),
+(3, 'Tools for Beginners'),
+(4, 'Wet Blocking'),
+(4, 'Treble 2 Together'),
+(4, 'Choosing your Yarn'),
+(5, 'Working into the Front Loop'),
+(5, 'Working into the Back Loop'),
+(5, 'Oversewn Seam'),
+(5, 'Oversewn Seam for Joining Blocks')
+;
