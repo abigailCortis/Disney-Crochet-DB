7348cec38513be52b821c3e2868893e8f16521dc
Abigail Cortis <cortisbiggirl@gmail.com>
2024-08-15 11:51:17 +0200

    minor schema changes

--- sql/V1__initial_schema.sql
+++ sql/V1__initial_schema.sql
@@ -4,26 +4,28 @@ CREATE TABLE throw(
     issue int NOT NULL,
     square_name text NOT NULL,
     colours text[] NOT NULL,
+    difficulty int NOT NULL,
 
     CONSTRAINT throw_pk PRIMARY KEY (issue, square_name),
     CHECK (issue > 0),
-    CHECK (array_length(colours, 1) > 0)
+    CHECK (array_length(colours, 1) > 0),
+    CHECK (difficulty > 0 AND difficulty <= 5)
 );
 
 CREATE TABLE small_projects(
     issue int NOT NULL,
-    "page" int NOT NULL,
     project_name text NOT NULL,
+    difficulty int NOT NULL,
 
     CONSTRAINT small_projects_pk PRIMARY KEY (project_name),
-    CHECK (issue > 0 AND "page" > 0)
+    CHECK (issue > 0),
+    CHECK (difficulty > 0 AND difficulty <= 5)
 );
 
 CREATE TABLE skills (
     issue int NOT NULL,
-    "page" int NOT NULL,
     skill text NOT NULL,
 
     CONSTRAINT skills_pk PRIMARY KEY (skill),
-    CHECK (issue > 0 AND "page" > 0)
+    CHECK (issue > 0)
 );
