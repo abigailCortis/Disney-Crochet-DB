6b0fd37b843e6cb186e809accdc1c81d97f84295
Abigail Cortis <cortisbiggirl@gmail.com>
2024-08-21 22:50:34 +0200

    added data for issue 9

--- sql/V3__issues6-10.sql
+++ sql/V3__issues6-10.sql
@@ -1,7 +1,8 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (6, 'Mickey and Minnie Yellow Flower', ARRAY['Vanilla', 'Sunflower'], 1),
 (7, 'Winnie the Pooh Square', ARRAY['Bright Red', 'White', 'Sunflower', 'Black'], 1),
-(8, 'Frozen Granny Square', ARRAY['Sky Blue', 'Cyan', 'Peacock Blue'], 1)
+(8, 'Frozen Granny Square', ARRAY['Sky Blue', 'Cyan', 'Peacock Blue'], 1),
+(9, 'Jungle Book Golden Flower Square', ARRAY['Sunflower'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -13,7 +14,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (7, 'Frozen Place Mats' 3),
 (8, 'Princess Cup Cosies', 1),
 (8, 'Finding Nemo Key Fob', 3),
-(8, 'Mickey Pouffe', 2)
+(8, 'Mickey Pouffe', 2),
+(9, 'Luca Friendship Bracelets', 2),
+(9, 'Cruella Fingerless Gloves', 3),
+(9, 'Brave Throw', 5)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -25,5 +29,7 @@ INSERT INTO skills (issue, skill) VALUES
 (7, 'Turning Chains'),
 (7, 'Joining a New Yarn'),
 (8, 'Alternate Stitch'),
-(8, 'Basic Spiral')
+(8, 'Basic Spiral'),
+(9, 'Cable Stitch'),
+(9, 'Working aroung the Post')
 ;
