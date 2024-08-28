67e3e11732fe956533b2d0b8de4ed618aacc0ff8
Abigail Cortis <cortisbiggirl@gmail.com>
2024-08-28 19:54:58 +0200

    added data for issues 11 and 12

---
+++ sql/V4__issues11-15.sql
@@ -0,0 +1,22 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(11, 'Nemo Aqua Flower', ARRAY['Aqua Green'], 1),
+(12, 'Winnie the Pooh Woodland Flower', ARRAY['Sunflower', 'Vanilla', 'Brick Red', 'White', 'Sage', 'Sea Green', 'Dark Teal'], 1),
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(11, 'Minnie Mouse Earrings', 1),
+(11, '101 Dalmations Jacket', 3),
+(11, 'Princess Luggage Tags', 2),
+(12, 'Eeyore Baby Slippers', 1),
+(12, 'Inside Out Stress Balls', 2),
+(12, 'Vibrant Coco Cushion', 3)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(11, 'Bobbles'),
+(11, 'Shaping 1'),
+(11, 'Spike Stitches 1'),
+(12, 'Double Crochet Cluster Stitch'),
+(12, 'Shaping 2'),
+(12, 'Finishing a Spiral')
+;
