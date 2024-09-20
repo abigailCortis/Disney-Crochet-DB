2f02c9903789e62d83c664f260a555f6368c192a
Abigail Cortis <cortisbiggirl@gmail.com>
2024-09-20 20:59:11 +0200

    added data for issue 14

--- sql/V4__issues11-15.sql
+++ sql/V4__issues11-15.sql
@@ -1,7 +1,8 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (11, 'Nemo Aqua Flower', ARRAY['Aqua Green'], 1),
 (12, 'Winnie the Pooh Woodland Flower', ARRAY['Sunflower', 'Vanilla', 'Brick Red', 'White', 'Sage', 'Sea Green', 'Dark Teal'], 1),
-(13, 'Minnie''s Bow', ARRAY['White', 'Sunflower', 'Cyan', 'Periwinkle'], 2)
+(13, 'Minnie''s Bow', ARRAY['White', 'Sunflower', 'Cyan', 'Periwinkle'], 2),
+(14, 'Mickey & Minnie Portraits', ARRAY['Peach', 'Black', 'White', 'Sky Blue', 'Cyan', 'Bright Red'], 3),
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -13,7 +14,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (12, 'Vibrant Coco Cushion', 3),
 (13, 'Ariel Shell Necklace', 1),
 (13, 'Buzz Baby Blanket', 1),
-(13, 'Minnie Mouse Tea Cosy', 3)
+(13, 'Minnie Mouse Tea Cosy', 3),
+(14, 'Cinderella Mice', 2),
+(14, 'Snow White Apple Sweater', 4),
+(14, 'Tigger Rug', 1)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -25,5 +29,7 @@ INSERT INTO skills (issue, skill) VALUES
 (12, 'Finishing a Spiral'),
 (13, 'Popcorn Stitch'),
 (13, 'Crossed Stitches 1'),
-(13, 'Spike Stitches 2')
+(13, 'Spike Stitches 2'),
+(14, 'Standing Chains'),
+(14, 'Crossed Stitches 2')
 ;
