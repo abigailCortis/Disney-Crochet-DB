820b58b24f2a346303bd7940f2f96b7e4bf0a72e
Abigail Cortis <cortisbiggirl@gmail.com>
2024-09-26 18:48:34 +0200

    added data for issue 16

---
+++ sql/V5__issues16-20.sql
@@ -0,0 +1,14 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(16, 'Tangled White Rose Square', ARRAY['Sunflower', 'White', 'Sea Green', 'Shell Pink', 'Candy Pink'], 3),
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(16, 'Moana Necklace', 2),
+(16, 'Minnie Mouse Circle Bag', 1),
+(16, 'Tangled Tower Doorstop', 2)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(16, 'Back and Front Loop Half Treble'),
+(16, 'Stitched Seams 1'),
+;
