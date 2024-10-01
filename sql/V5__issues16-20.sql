f2627a93617acfc971c473542ec74eb8bffec1ee
Abigail Cortis <cortisbiggirl@gmail.com>
2024-10-01 20:05:49 +0200

    added data for issue 18

--- sql/V5__issues16-20.sql
+++ sql/V5__issues16-20.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (16, 'Tangled White Rose Square', ARRAY['Sunflower', 'White', 'Sea Green', 'Shell Pink', 'Candy Pink'], 3),
 (17, 'Donald Duck Portrait Square', ARRAY['Vanilla', 'Sunflower', 'White', 'Black', 'Royal Blue', 'Bright Red'], 3),
+(18, 'Cinderella Spicy Pumpkin Square', ARRAY['Brick Red', 'Paprika'], 2),
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -10,11 +11,16 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (17, 'Belle Rose', 2)
 (17, 'Jungle Book Crossbody Bag', 1),
 (17, 'Nemo Picture Cushion', 2),
+(18, 'Tangled Hair Combs', 2),
+(18, 'Sulley Sweater', 2),
+(18, 'Winnie the Pooh Pot Cover', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (16, 'Back and Front Loop Half Treble'),
 (16, 'Stitched Seams 1'),
-(17, 'Simple Openwork Mesh')
+(17, 'Simple Openwork Mesh'),
 (17, 'Shell Stitches'),
+(18, 'Quadruple Treble'),
+(18, 'Stitched Seams 2')
 ;
