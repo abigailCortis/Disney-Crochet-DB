2ddfcb4e4d588e9df9156a9e340fefbe459d8f1b
Abigail Cortis <cortisbiggirl@gmail.com>
2024-10-02 22:07:31 +0200

    added data for issue 19

--- sql/V5__issues16-20.sql
+++ sql/V5__issues16-20.sql
@@ -2,6 +2,7 @@ INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (16, 'Tangled White Rose Square', ARRAY['Sunflower', 'White', 'Sea Green', 'Shell Pink', 'Candy Pink'], 3),
 (17, 'Donald Duck Portrait Square', ARRAY['Vanilla', 'Sunflower', 'White', 'Black', 'Royal Blue', 'Bright Red'], 3),
 (18, 'Cinderella Spicy Pumpkin Square', ARRAY['Brick Red', 'Paprika'], 2),
+(19, 'Mickey & Minnie Flower Patch', ARRAY['Periwinkle', 'Candy Pink', 'Sunflower', 'Coral', 'Bright Red', 'Cyan', 'Vanilla', 'White'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -13,7 +14,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (17, 'Nemo Picture Cushion', 2),
 (18, 'Tangled Hair Combs', 2),
 (18, 'Sulley Sweater', 2),
-(18, 'Winnie the Pooh Pot Cover', 2)
+(18, 'Winnie the Pooh Pot Cover', 2),
+(19, 'Dalmation-spotted Ear Warmer', 2),
+(19, 'Anna Bonnet', 3),
+(19, 'Cinderella Trinket Pot', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -22,5 +26,7 @@ INSERT INTO skills (issue, skill) VALUES
 (17, 'Simple Openwork Mesh'),
 (17, 'Shell Stitches'),
 (18, 'Quadruple Treble'),
-(18, 'Stitched Seams 2')
+(18, 'Stitched Seams 2'),
+(19, 'Woven Stitch'),
+(19, 'Puff Stitch in the Round')
 ;
