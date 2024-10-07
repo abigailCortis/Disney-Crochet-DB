30ef2ecad97d57c5962e97460446955719aaea90
Abigail Cortis <cortisbiggirl@gmail.com>
2024-10-07 22:01:44 +0200

    added data for issue 20

--- sql/V5__issues16-20.sql
+++ sql/V5__issues16-20.sql
@@ -2,7 +2,9 @@ INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (16, 'Tangled White Rose Square', ARRAY['Sunflower', 'White', 'Sea Green', 'Shell Pink', 'Candy Pink'], 3),
 (17, 'Donald Duck Portrait Square', ARRAY['Vanilla', 'Sunflower', 'White', 'Black', 'Royal Blue', 'Bright Red'], 3),
 (18, 'Cinderella Spicy Pumpkin Square', ARRAY['Brick Red', 'Paprika'], 2),
-(19, 'Mickey & Minnie Flower Patch', ARRAY['Periwinkle', 'Candy Pink', 'Sunflower', 'Coral', 'Bright Red', 'Cyan', 'Vanilla', 'White'], 3)
+(19, 'Mickey & Minnie Flower Patch', ARRAY['Periwinkle', 'Candy Pink', 'Sunflower', 'Coral', 'Bright Red', 'Cyan', 'Vanilla', 'White'], 3),
+(20, 'Finding Nemo Sheldon & Peach Background Squares', ARRAY['Ice Blue', 'Aqua Green', 'Cyan', 'Peacock Blue'], 1),
+(20, 'Adding the White Borders', ARRAY['White'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -17,7 +19,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (18, 'Winnie the Pooh Pot Cover', 2),
 (19, 'Dalmation-spotted Ear Warmer', 2),
 (19, 'Anna Bonnet', 3),
-(19, 'Cinderella Trinket Pot', 3)
+(19, 'Cinderella Trinket Pot', 3),
+(20, 'Ariel Sponges', 2),
+(20, 'Brave Circular Shopper', 3),
+(20, 'Briar Rose Crib Blanket', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -28,5 +33,7 @@ INSERT INTO skills (issue, skill) VALUES
 (18, 'Quadruple Treble'),
 (18, 'Stitched Seams 2'),
 (19, 'Woven Stitch'),
-(19, 'Puff Stitch in the Round')
+(19, 'Puff Stitch in the Round'),
+(20, 'Fan Stitch'),
+(20, 'Cluster Stitches 2')
 ;
