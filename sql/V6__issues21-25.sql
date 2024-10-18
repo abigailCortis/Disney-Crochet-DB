fe97a72e18cf5c9963147bf791f95dedff6f2484
Abigail Cortis <cortisbiggirl@gmail.com>
2024-10-19 00:54:25 +0200

    added data for issue 23

--- sql/V6__issues21-25.sql
+++ sql/V6__issues21-25.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (21, 'Tangled Pink Heart Square', ARRAY['White', 'Vanilla', 'Lime', 'Candy Pink'], 2),
 (22, 'Finding Nemo Peach Character Square & Star Square', ARRAY['Candy Pink', 'Brick Red', 'Black', 'White'], 3),
+(23, 'Winnie the Pooh Balloon Square', ARRAY['Dark Teal', 'Sky Blue', 'White', 'Sunflower', 'Black'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -9,10 +10,16 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (21, 'Bambi Storage Basket', 3),
 (22, 'Anna Fingerless Mitts', 1),
 (22, 'Bonnie Pencil Case', 3,
-(22, 'Luca Gelato Bunting', 2))
+(22, 'Luca Gelato Bunting', 2)),
+(23, 'Daisy Duck''s Fortune Cookies', 1),
+(23, 'Frozen Baby Booties', 3),
+(23, 'Nemo Towel', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (21, 'Crocodile Stitch'),
-(22, 'French Knots')
+(22, 'French Knots'),
+(23, 'Cobble Stitch'),
+(23, 'Corner to Corner'),
+(23, 'Crochet Seams 1')
 ;
