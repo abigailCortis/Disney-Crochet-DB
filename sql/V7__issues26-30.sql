aacadc637574a07f6d1885f5f2c821318dfcdc50
Abigail Cortis <cortisbiggirl@gmail.com>
2024-11-03 14:54:06 +0100

    added data for issue 27

--- sql/V7__issues26-30.sql
+++ sql/V7__issues26-30.sql
@@ -1,14 +1,21 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (26, 'Aladdin Portrait Square', ARRAY['Fudge', 'Cyan', 'Ice Blue', 'White', 'Black', 'Bright Red'], 2),
+(27, 'Cinderella Carriage Wheetl Square', ARRAY['Shell Pink', 'Petal', 'Coral', 'Lime', 'Aqua Green', 'White', 'Brick Red'], 2)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (26, 'Remy Potholder', 2),
 (26, 'Ariel Pencil Case', 3),
-(26, 'Wreck-It Ralph''s Candy Stripe Cushion', 4)
+(26, 'Wreck-It Ralph''s Candy Stripe Cushion', 4),
+(27, 'Bluebird Hanger', 1),
+(27, 'Pocahontas-inspired Booties', 3),
+(27, 'Frozen Doily', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (26, 'Crab Stitch'),
-(26, 'Colourwork 1')
+(26, 'Colourwork 1'),
+(27, 'Close Shell Stitch'),
+(27, 'Puff Stitches'),
+(27, 'Crochet Seams 2')
 ;
