a12ec424b4e8723ac1b952b466163eee90719ee1
Abigail Cortis <cortisbiggirl@gmail.com>
2024-11-05 18:51:56 +0100

    added data for issue 28

--- sql/V7__issues26-30.sql
+++ sql/V7__issues26-30.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (26, 'Aladdin Portrait Square', ARRAY['Fudge', 'Cyan', 'Ice Blue', 'White', 'Black', 'Bright Red'], 2),
-(27, 'Cinderella Carriage Wheetl Square', ARRAY['Shell Pink', 'Petal', 'Coral', 'Lime', 'Aqua Green', 'White', 'Brick Red'], 2)
+(27, 'Cinderella Carriage Wheetl Square', ARRAY['Shell Pink', 'Petal', 'Coral', 'Lime', 'Aqua Green', 'White', 'Brick Red'], 2),
+(28, 'Coco Pink Paper Cut Square', ARRAY['Hot Pink', 'White'], 2),
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -9,7 +10,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (26, 'Wreck-It Ralph''s Candy Stripe Cushion', 4),
 (27, 'Bluebird Hanger', 1),
 (27, 'Pocahontas-inspired Booties', 3),
-(27, 'Frozen Doily', 2)
+(27, 'Frozen Doily', 2),
+(28, 'Bambi Flower Mats', 2),
+(28, 'Lilo & Stitch Beach Bag', 2),
+(28, 'Mrs Potts Tea Cosy', 4)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -17,5 +21,7 @@ INSERT INTO skills (issue, skill) VALUES
 (26, 'Colourwork 1'),
 (27, 'Close Shell Stitch'),
 (27, 'Puff Stitches'),
-(27, 'Crochet Seams 2')
+(27, 'Crochet Seams 2'),
+(28, 'Simple Wave Stitch'),
+(28, 'Colourwork 2')
 ;
