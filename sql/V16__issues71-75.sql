bdfcab2a07cdba0aa1dcfc0c7e7a137db8033095
Abigail Cortis <cortisbiggirl@gmail.com>
2024-11-13 19:00:32 +0100

    added data for issue 75

---
+++ sql/V16__issues71-75.sql
@@ -0,0 +1,13 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(75, 'Aladdin Clematis Flower Square', ARRAY['Sea Green', 'Hibiscus', 'Fuchsia', 'Periwinkle', 'White', 'Aqua Green'], 3)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(75, 'Turning Red Miriam-inspired beanie', 1),
+(75, 'Stepsisters-inspired cupcake pincushions', 3),
+(75, 'The Princess and the Frog Child''s Frog Hat', 2)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(75, 'Joining Hexagon Shapes 2')
+;
