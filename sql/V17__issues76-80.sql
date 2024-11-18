b1d8c751faab94ca75236406d7af2202f955dea7
Abigail Cortis <cortisbiggirl@gmail.com>
2024-11-18 20:31:52 +0100

    added data for issue 76

---
+++ sql/V17__issues76-80.sql
@@ -0,0 +1,13 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(76, 'Donald & Daisy Blue Lace Square', ARRAY['Sky Blue', 'White'], 2)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(76, 'Wall-E Eve Leaf Cushion', 2),
+(76, 'Big Hero 6 Baymax Tote Bag', 2),
+(76, 'Tangled Tealight Jar Covers', 3)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(76, 'Following a Pattern 1')
+;
