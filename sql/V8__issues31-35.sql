1f31bbbdfd623f969de7a1f8b5b83194ac5c2b98
Abigail Cortis <cortisbiggirl@gmail.com>
2024-11-19 18:35:37 +0100

    added data for issue 32

--- sql/V8__issues31-35.sql
+++ sql/V8__issues31-35.sql
@@ -1,14 +1,21 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (31, 'Daisy Duck Portrait Square', ARRAY['Sky Blue', 'Sunflower', 'Royal Blue', 'White', 'Black', 'Hibiscus'], 2),
 (31, 'Winnie the Pooh Honey Square', ARRAY['Sunflower', 'White'], 1),
+(32, 'Spice Flower', ARRAY['Paprika', 'White', 'Sunflower', 'Fuchsia', 'Lime'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (31, 'Snow White Scrubbies', 1),
 (31, 'Mulan Artist Brush Holder', 3),
-(31, 'Cars Tyre Pouffe', 3)
+(31, 'Cars Tyre Pouffe', 3),
+(32, 'Esmeralda Bangle', 3),
+(32, 'Luca Coin Purse', 3),
+(32, 'Pocahontas Harvest Baskets', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (31, 'Rounded Picot Stitch'),
+(32, 'French Picot Square'),
+(32, 'Cross Stitch on Crochet'),
+(32, 'Buttonholes 1'),
 ;
