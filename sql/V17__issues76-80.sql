8738e7e9ec262da7993e0dccc50b1f615d9b6b83
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-02 21:20:48 +0100

    added data for issue 78

--- sql/V17__issues76-80.sql
+++ sql/V17__issues76-80.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (76, 'Donald & Daisy Blue Lace Square', ARRAY['Sky Blue', 'White'], 2),
-(77, 'Cinderella Pumpkin Patch Square', ARRAY['Vanilla', 'Coral', 'Paprika', 'Sea Green', 'Sage', 'White'], 2)
+(77, 'Cinderella Pumpkin Patch Square', ARRAY['Vanilla', 'Coral', 'Paprika', 'Sea Green', 'Sage', 'White'], 2),
+(78, 'Belle Portrait Square', ARRAY['Shell Pink', 'Sunflower', 'Sky Blue', 'White', 'Toasted Brown', 'Black', 'Vanilla'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -9,11 +10,16 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (76, 'Tangled Tealight Jar Covers', 3),
 (77, 'Bambi Flower Coasters', 1),
 (77, 'Ursula Shell Earrings', 2),
-(77, 'The Emperor''s New Groove Napkin Rings', 2)
+(77, 'The Emperor''s New Groove Napkin Rings', 2),
+(78, 'Up Balloon Cake Toppers', 1),
+(78, 'The Little Mermaid Net Shopper', 2),
+(78, 'Mickey & Minnie Mug Cosies', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (76, 'Following a Pattern 1'),
 (77, 'Open Scallops Stitch'),
-(77, 'Following a Pattern 2')
+(77, 'Following a Pattern 2'),
+(78, 'Two-Colour Shell Stitch'),
+(78, 'Making Pockets 1')
 ;
