5914f26568e63ecde061d733d05bbdfcf4954a68
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-04 19:28:36 +0100

    added data for issue 35

--- sql/V8__issues31-35.sql
+++ sql/V8__issues31-35.sql
@@ -3,7 +3,8 @@ INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (31, 'Winnie the Pooh Honey Square', ARRAY['Sunflower', 'White'], 1),
 (32, 'Spice Flower', ARRAY['Paprika', 'White', 'Sunflower', 'Fuchsia', 'Lime'], 3),
 (33, 'Jungle Book Jungle Bouquet Square', ARRAY['Vanilla', 'Sage', 'White', 'Paprika', 'Coral', 'Sunflower'], 2),
-(34, 'Mickey & Minnie Popcorn Daisy Square', ARRAY['Sunflower', 'White', 'Periwinkle'], 3)
+(34, 'Mickey & Minnie Popcorn Daisy Square', ARRAY['Sunflower', 'White', 'Periwinkle'], 3),
+(35, 'Red Apple Square', ARRAY['Petal', 'White', 'Bright Red', 'Fudge', 'Sage'], 2)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -19,6 +20,9 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (34, 'Peter Pan Slipper Socks', 2),
 (34, 'Cinderella Colour Change Shawl', 4),
 (34, 'Pocahontas Cushion', 3),
+(35, 'Yzma Zigzag Scarf', 2),
+(35, 'wreck-It Ralph Candy Hair Clips', 2),
+(35, 'Three Little Pigs Tissue Box Cover', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -31,5 +35,8 @@ INSERT INTO skills (issue, skill) VALUES
 (33, 'Satin Stitch on Crochet'),
 (33, 'Buttonholes 2'),
 (34, 'Treble 4 to 7 Together'),
-(34, 'Fringes')
+(34, 'Fringes'),
+(35, 'Rib Stitch'),
+(35, 'Open Chevron Stitch'),
+(35, 'Making Buttons 1')
 ;
