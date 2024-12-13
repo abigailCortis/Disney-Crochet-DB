604e91e81400b6a4dabc847a0062641990816e16
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-13 23:20:36 +0100

    added data for issue 38

--- sql/V9__issues36-40.sql
+++ sql/V9__issues36-40.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (36, 'Coco Fiesta Flower Square', ARRAY['Sunflower', 'Brick Red', 'Hot Pink', 'Black', 'Cyan', 'White'], 2),
-(37, 'Ariel Portrait Square', ARRAY['Shell Pink', 'Aqua Green', 'Ice Blue', 'White', 'Brick Red', 'Sunflower', 'Black', Paprika], 3)
+(37, 'Ariel Portrait Square', ARRAY['Shell Pink', 'Aqua Green', 'Ice Blue', 'White', 'Brick Red', 'Sunflower', 'Black', Paprika], 3),
+(38, 'Coral Reef Square', ARRAY['Coral', 'White'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -9,12 +10,16 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (36, 'Hercules Snake Toy', 2),
 (37, 'Belle Hooded Scarf', 2),
 (37, 'Edna Mode Stylish Case', 3),
-(37, 'Finding Dory Home Letters', 3)
+(37, 'Finding Dory Home Letters', 3),
+(38, 'Winnie the Pooh Pooh Corner Bunting', 1),
+(38, 'Aurora Rose Bag', 3),
+(38, 'Picnic Play Set', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (36, 'Spider Stitch'),
 (36, 'Making Buttons 2'),
 (37, 'Three-Colour Fan Stitch'),
-(37, 'Making Tassels')
+(37, 'Making Tassels'),
+(38, 'Crochet with Beads 1')
 ;
