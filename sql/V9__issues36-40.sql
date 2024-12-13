64009f9991d88780cc7dd075401a0d410c14fe55
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-13 11:05:28 +0100

    added data for issue 37

--- sql/V9__issues36-40.sql
+++ sql/V9__issues36-40.sql
@@ -1,14 +1,20 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
-(36, 'Coco Fiesta Flower Square', ARRAY['Sunflower', 'Brick Red', 'Hot Pink', 'Black', 'Cyan', 'White'], 2)
+(36, 'Coco Fiesta Flower Square', ARRAY['Sunflower', 'Brick Red', 'Hot Pink', 'Black', 'Cyan', 'White'], 2),
+(37, 'Ariel Portrait Square', ARRAY['Shell Pink', 'Aqua Green', 'Ice Blue', 'White', 'Brick Red', 'Sunflower', 'Black', Paprika], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (36, 'Turning Red Panda ears', 1),
 (36, 'Winnie the Pooh Trio of Socks', 2),
-(36, 'Hercules Snake Toy', 2)
+(36, 'Hercules Snake Toy', 2),
+(37, 'Belle Hooded Scarf', 2),
+(37, 'Edna Mode Stylish Case', 3),
+(37, 'Finding Dory Home Letters', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (36, 'Spider Stitch'),
-(36, 'Making Buttons 2')
+(36, 'Making Buttons 2'),
+(37, 'Three-Colour Fan Stitch'),
+(37, 'Making Tassels')
 ;
