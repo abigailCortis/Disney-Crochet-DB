3e7290cbfad9e44581220501a8aa7b37edee54cc
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-27 12:28:46 +0100

    added data for issue 80

--- sql/V17__issues76-80.sql
+++ sql/V17__issues76-80.sql
@@ -3,7 +3,9 @@ INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (77, 'Cinderella Pumpkin Patch Square', ARRAY['Vanilla', 'Coral', 'Paprika', 'Sea Green', 'Sage', 'White'], 2),
 (78, 'Belle Portrait Square', ARRAY['Shell Pink', 'Sunflower', 'Sky Blue', 'White', 'Toasted Brown', 'Black', 'Vanilla'], 3),
 (79, 'Aladdin Granny Square', ARRAY['Vanilla', 'Paprika', 'Candy Pink', 'Hot Pink', 'Ice Blue', 'Cyan', 'Periwinkle', 'White'], 1),
-(79, 'Donald & Daisy Granny Square', ARRAY['Sunflower', 'Sky Blue', 'Royal Blue', 'Vanilla', 'White'], 1)
+(79, 'Donald & Daisy Granny Square', ARRAY['Sunflower', 'Sky Blue', 'Royal Blue', 'Vanilla', 'White'], 1),
+(80, 'Frozen Swirling Snowflake Square', ARRAY['Ice Blue', 'Cyan', 'Perwinkle', 'White'], 1),
+(80, 'Frozen Dancing Snowflake Square', ARRAY['Sky Blue', 'Ice Blue', 'Aqua Green', 'White'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -18,7 +20,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (78, 'Mickey & Minnie Mug Cosies', 2), 
 (79, 'Olaf Toothbrush Case', 1),
 (79, 'Miss Bianca-inspired Bag', 2),
-(79, 'Luca Gelato Ball and Cone Game', 2)
+(79, 'Luca Gelato Ball and Cone Game', 2),
+(80, 'Encanto Jug Cover', 2),
+(80, 'Meeko Pocket Scarf', 3),
+(80, 'Toy Story Cutlery Roll', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -27,5 +32,6 @@ INSERT INTO skills (issue, skill) VALUES
 (77, 'Following a Pattern 2'),
 (78, 'Two-Colour Shell Stitch'),
 (78, 'Making Pockets 1'),
-(79, 'Making Pockets 2')
+(79, 'Making Pockets 2'),
+(80, 'Making Pockets 3')
 ;
