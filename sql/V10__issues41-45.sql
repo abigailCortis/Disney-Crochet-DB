076c2cdba1a01be9228dc1c35c965b1c4924642e
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-01 21:32:11 +0100

    added data for issue 42

--- sql/V10__issues41-45.sql
+++ sql/V10__issues41-45.sql
@@ -1,15 +1,22 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
-(41, 'Aladdin Magic Flower Square', ARRAY['Vanilla', 'Coral', 'Hot Pink', 'Periwinkle', 'Aqua Green', 'White'], 2)
+(41, 'Aladdin Magic Flower Square', ARRAY['Vanilla', 'Coral', 'Hot Pink', 'Periwinkle', 'Aqua Green', 'White'], 2),
+(42, 'Cinderella Pumpkin Square', ARRAY['Sky Blue', 'White', 'Paprika', 'Sage', 'Sea Green'], 2)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (41, 'Piglet Baby Booties', 1),
 (41, 'Inside Out Spotty Purse', 3),
-(41, 'Lilo Star and Moon Herb Bags', 2)
+(41, 'Lilo Star and Moon Herb Bags', 2),
+(42, 'Inside Out Coasters', 1),
+(42, 'Wreck-It Ralph Strawberry Hat', 1),
+(42, 'Andy''s Room Storage Basket', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (41, 'Small Daisy Stitch'),
 (41, 'Vine Stitch'),
-(41, 'Surface Crochet')
+(41, 'Surface Crochet'),
+(42, 'Raised Popcorns'),
+(42, 'Harlequin Stitch'),
+(42, 'Coloured Tweed Stitch')
 ;
