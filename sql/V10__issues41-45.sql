e43335bdd1f2812ab8290d038836a65bd4ade125
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-03 23:02:41 +0100

    added data for issue 43

--- sql/V10__issues41-45.sql
+++ sql/V10__issues41-45.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (41, 'Aladdin Magic Flower Square', ARRAY['Vanilla', 'Coral', 'Hot Pink', 'Periwinkle', 'Aqua Green', 'White'], 2),
-(42, 'Cinderella Pumpkin Square', ARRAY['Sky Blue', 'White', 'Paprika', 'Sage', 'Sea Green'], 2)
+(42, 'Cinderella Pumpkin Square', ARRAY['Sky Blue', 'White', 'Paprika', 'Sage', 'Sea Green'], 2),
+(43, 'Aladdin Pastel Flower Square', ARRAY['Hibiscus', 'White'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -9,7 +10,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (41, 'Lilo Star and Moon Herb Bags', 2),
 (42, 'Inside Out Coasters', 1),
 (42, 'Wreck-It Ralph Strawberry Hat', 1),
-(42, 'Andy''s Room Storage Basket', 3)
+(42, 'Andy''s Room Storage Basket', 3),
+(43, 'Encanto Hair Scrunshies', 1),
+(43, 'Oliver and Company Piano Scarf', 3),
+(43, 'Mike Doorstop', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -18,5 +22,7 @@ INSERT INTO skills (issue, skill) VALUES
 (41, 'Surface Crochet'),
 (42, 'Raised Popcorns'),
 (42, 'Harlequin Stitch'),
-(42, 'Coloured Tweed Stitch')
+(42, 'Coloured Tweed Stitch'),
+(43, 'Bar and Lattice Stitch'),
+(43, 'Shaping 4')
 ;
