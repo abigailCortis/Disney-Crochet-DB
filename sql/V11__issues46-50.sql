f352bd7484355900b526129df9097b96dcbdb86a
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-11 15:46:22 +0100

    added data for issue 46

---
+++ sql/V11__issues46-50.sql
@@ -0,0 +1,15 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(46, 'Frozen Icicle Square', ARRAY['Cyan', 'White', 'Ice Blue', 'Periwinkle'], 3)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(46, 'Cinderella Pumpkin Decoration', 1),
+(46, 'Mike Beanie Hat', 2),
+(46, 'Cheshire Cat Hook Store', 3)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(46, 'Arch and Grid Stitch'),
+(46, 'Trellis & Puff Stitch'),
+(46, 'Fan Stitch')
+;
