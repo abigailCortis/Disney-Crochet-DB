f694e693fa0b5da4974a3766fbc83eb8a9f87bad
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-12 13:22:41 +0100

    added data for issue 47

--- sql/V11__issues46-50.sql
+++ sql/V11__issues46-50.sql
@@ -1,15 +1,21 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
-(46, 'Frozen Icicle Square', ARRAY['Cyan', 'White', 'Ice Blue', 'Periwinkle'], 3)
+(46, 'Frozen Icicle Square', ARRAY['Cyan', 'White', 'Ice Blue', 'Periwinkle'], 3),
+(47, 'Tangled Damask Rose Square', ARRAY['Candy Pink', 'White'], 2)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (46, 'Cinderella Pumpkin Decoration', 1),
 (46, 'Mike Beanie Hat', 2),
-(46, 'Cheshire Cat Hook Store', 3)
+(46, 'Cheshire Cat Hook Store', 3),
+(47, 'The Princess & the Frog Lily-Pad Trivet', 2),
+(47, 'Simba Mittens', 3),
+(47, 'Mulan Yoga Mat Bag', 4)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (46, 'Arch and Grid Stitch'),
 (46, 'Trellis & Puff Stitch'),
-(46, 'Fan Stitch')
+(46, 'Fan Stitch'),
+(47, 'Offset Raised Double Treble'),
+(47, 'Working in the Round 1')
 ;
