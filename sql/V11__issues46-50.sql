c8fd6cd2de0ba8a071f52f390d10d21c519cee2c
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-14 18:44:57 +0100

    added data for issue 49

--- sql/V11__issues46-50.sql
+++ sql/V11__issues46-50.sql
@@ -2,6 +2,7 @@ INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (46, 'Frozen Icicle Square', ARRAY['Cyan', 'White', 'Ice Blue', 'Periwinkle'], 3),
 (47, 'Tangled Damask Rose Square', ARRAY['Candy Pink', 'White'], 2),
 (48, 'Beauty & the Beast Belle Granny Square', ARRAY['Coral', 'Vanilla', 'Sky Blue', 'Petal', 'Bright Red', 'Royal Blue', 'White'], 1),
+(49, 'Cinderella Portrait Square', ARRAY['Shell Pink', 'Ice Blue', 'Coral', 'White', 'Cyan', 'Sunflower', 'Periwinkle', 'Black'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -13,7 +14,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (47, 'Mulan Yoga Mat Bag', 4),
 (48, 'Belle''s Hair Bow', 1),
 (48, 'Luca Fisherman''s Flat Cap', 2),
-(48, 'Sea you Soon Crab Friend', 2)
+(48, 'Sea you Soon Crab Friend', 2),
+(49, 'Toy Story Floor Protectors', 1),
+(49, 'Trio of Kittens', 2),
+(49, 'Pocahontas Drawstring Pouch', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -24,5 +28,7 @@ INSERT INTO skills (issue, skill) VALUES
 (47, 'Working in the Round 1'),
 (48, 'Framed Square Motif'),
 (48, 'Diagonal Picot Mesh'),
-(48, 'Working in the Round 2')
+(48, 'Working in the Round 2'),
+(49, 'Pointed Petal Flower Motif'),
+(49, 'Tubular Crochet 1')
 ;
