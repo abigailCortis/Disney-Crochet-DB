8b64e4ef2d6dd517cfd5e02e6cde639361c85353
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-18 19:23:05 +0100

    added data for issue 50

--- sql/V11__issues46-50.sql
+++ sql/V11__issues46-50.sql
@@ -2,7 +2,8 @@ INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (46, 'Frozen Icicle Square', ARRAY['Cyan', 'White', 'Ice Blue', 'Periwinkle'], 3),
 (47, 'Tangled Damask Rose Square', ARRAY['Candy Pink', 'White'], 2),
 (48, 'Beauty & the Beast Belle Granny Square', ARRAY['Coral', 'Vanilla', 'Sky Blue', 'Petal', 'Bright Red', 'Royal Blue', 'White'], 1),
-(49, 'Cinderella Portrait Square', ARRAY['Shell Pink', 'Ice Blue', 'Coral', 'White', 'Cyan', 'Sunflower', 'Periwinkle', 'Black'], 3)
+(49, 'Cinderella Portrait Square', ARRAY['Shell Pink', 'Ice Blue', 'Coral', 'White', 'Cyan', 'Sunflower', 'Periwinkle', 'Black'], 3),
+(50, 'Donald & Daisy Sunflower Square', ARRAY['Sunflower', 'White'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -17,7 +18,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (48, 'Sea you Soon Crab Friend', 2),
 (49, 'Toy Story Floor Protectors', 1),
 (49, 'Trio of Kittens', 2),
-(49, 'Pocahontas Drawstring Pouch', 2)
+(49, 'Pocahontas Drawstring Pouch', 2),
+(50, 'Frozen Snowflake Decorations', 1),
+(50, 'Encanto Bobble Cowl', 2),
+(50, 'Wreck-It Ralph Candy Cane Wand', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -30,5 +34,7 @@ INSERT INTO skills (issue, skill) VALUES
 (48, 'Diagonal Picot Mesh'),
 (48, 'Working in the Round 2'),
 (49, 'Pointed Petal Flower Motif'),
-(49, 'Tubular Crochet 1')
+(49, 'Tubular Crochet 1'),
+(50, 'Espaliered Fans'),
+(50, 'Changing Yarn and Weaving In Ends')
 ;
