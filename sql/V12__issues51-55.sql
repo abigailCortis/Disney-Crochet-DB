f5e6df0c3d0b6ca12631c108b3dfc90e6368ade5
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-19 15:46:17 +0100

    added data for issue 51

---
+++ sql/V12__issues51-55.sql
@@ -0,0 +1,16 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(51, 'Donald & Daisy Posy Square', ARRAY['Sunflower', 'Sky Blue', 'White', 'Sage', 'Candy Pink', 'Bright Red', 'Royal Blue'], 3)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(51, 'Ratatouille Strawberry Coasters', 1),
+(51, 'Belle Evening Clutch Bag', 4),
+(51, 'Tangled Cushion Cover', 3)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(51, 'Petal and Scallop Motif'),
+(51, 'Measuring Tension')
+(51, 'Changing Hooks'),
+(51, 'Making Buttons 3')
+;
