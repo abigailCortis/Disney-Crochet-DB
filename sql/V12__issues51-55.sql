19192617c26069174218a8400d0804f8215b18b9
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-25 14:24:31 +0100

    added data for issue 52

--- sql/V12__issues51-55.sql
+++ sql/V12__issues51-55.sql
@@ -1,16 +1,23 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
-(51, 'Donald & Daisy Posy Square', ARRAY['Sunflower', 'Sky Blue', 'White', 'Sage', 'Candy Pink', 'Bright Red', 'Royal Blue'], 3)
+(51, 'Donald & Daisy Posy Square', ARRAY['Sunflower', 'Sky Blue', 'White', 'Sage', 'Candy Pink', 'Bright Red', 'Royal Blue'], 3),
+(52, 'Cinderella Pumpkin Leaves Square', ARRAY['Sea Green', 'Sage', 'White'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (51, 'Ratatouille Strawberry Coasters', 1),
 (51, 'Belle Evening Clutch Bag', 4),
-(51, 'Tangled Cushion Cover', 3)
+(51, 'Tangled Cushion Cover', 3),
+(52, 'Tangled Fairy Lights', 1),
+(52, '101 Dalmations Anita-Inspired Hat', 2),
+(52, 'Winnie the Pooh Curtain Tiebacks', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (51, 'Petal and Scallop Motif'),
 (51, 'Measuring Tension')
 (51, 'Changing Hooks'),
-(51, 'Making Buttons 3')
+(51, 'Making Buttons 3'),
+(52, 'Stiffening Crochet for Decorations'),
+(52, 'Popcorn Stitches'),
+(52, 'Bobble Stitches')
 ;
