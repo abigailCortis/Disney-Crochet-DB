f4d4aecd9fef3ef3dc3e575be3bc2407d017a4c8
Abigail Cortis <cortisbiggirl@gmail.com>
2025-01-26 12:01:16 +0100

    added data for issue 53

--- sql/V12__issues51-55.sql
+++ sql/V12__issues51-55.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (51, 'Donald & Daisy Posy Square', ARRAY['Sunflower', 'Sky Blue', 'White', 'Sage', 'Candy Pink', 'Bright Red', 'Royal Blue'], 3),
-(52, 'Cinderella Pumpkin Leaves Square', ARRAY['Sea Green', 'Sage', 'White'], 1)
+(52, 'Cinderella Pumpkin Leaves Square', ARRAY['Sea Green', 'Sage', 'White'], 1),
+(53, 'Jungle Book Jungle Flower Square', ARRAY['Coral', 'Vanilla', 'Sunflower', 'Lime', 'Sea Green', 'Sage', 'White'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
@@ -9,7 +10,10 @@ INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (51, 'Tangled Cushion Cover', 3),
 (52, 'Tangled Fairy Lights', 1),
 (52, '101 Dalmations Anita-Inspired Hat', 2),
-(52, 'Winnie the Pooh Curtain Tiebacks', 3)
+(52, 'Winnie the Pooh Curtain Tiebacks', 3),
+(53, 'The Rescuers Umbrella Cover', 1),
+(53, 'Tinker Bell Legwarmers', 2),
+(53, 'Snow White Dust Buster Mitt', 3)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -19,5 +23,8 @@ INSERT INTO skills (issue, skill) VALUES
 (51, 'Making Buttons 3'),
 (52, 'Stiffening Crochet for Decorations'),
 (52, 'Popcorn Stitches'),
-(52, 'Bobble Stitches')
+(52, 'Bobble Stitches'),
+(53, 'Scallop Stitch'),
+(53, 'Filet Crochet 2'),
+(53, 'Filet Crochet 3')
 ;
