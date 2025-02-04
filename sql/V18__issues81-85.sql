a0b0a5e4a81aa7c83d0057fd3f9f25147cdad807
Abigail Cortis <cortisbiggirl@gmail.com>
2025-02-04 19:19:31 +0100

    added data for issue 81

---
+++ sql/V18__issues81-85.sql
@@ -0,0 +1,13 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(81, 'Coco Coral Paper Cut Square', ARRAY['Coral', 'White'], 2),
+(81, 'The Jungle Book Lime Flower Square', ARRAY['Lime', 'White'], 2)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty) VALUES
+(81, 'Princess Jasmine Coin Purse', 2),
+(81, 'Yzma Cat-Pocket Jumper', 2,),
+(81, 'Cruella''s Fluffy Bag', 2)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+;
