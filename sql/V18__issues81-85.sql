d59999282fb1e4e9d9c7843629b0b88996e32c2a
Abigail Cortis <cortisbiggirl@gmail.com>
2025-02-10 12:22:10 +0100

    added data for issue 82

--- sql/V18__issues81-85.sql
+++ sql/V18__issues81-85.sql
@@ -1,13 +1,20 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (81, 'Coco Coral Paper Cut Square', ARRAY['Coral', 'White'], 2),
-(81, 'The Jungle Book Lime Flower Square', ARRAY['Lime', 'White'], 2)
+(81, 'The Jungle Book Lime Flower Square', ARRAY['Lime', 'White'], 2),
+(82, 'Donald & Daisy A Daisy for Daisy Square', ARRAY['Sunflower', 'White', 'Royal Blue', 'Hibiscus'], 2),
+(82, 'Joining The Donald & Daisy Block', ARRAY['White', 'Periwinkle'], 1)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty) VALUES
 (81, 'Princess Jasmine Coin Purse', 2),
 (81, 'Yzma Cat-Pocket Jumper', 2,),
-(81, 'Cruella''s Fluffy Bag', 2)
+(81, 'Cruella''s Fluffy Bag', 2),
+(82, 'Princess Friendship Bracelets', 1),
+(82, 'Mad Hatter''s Hat', 2),
+(82, 'Mulan Bunting', 2)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
+(82, 'Making Pockets 4'),
+(82, 'Making Pockets 5')
 ;
