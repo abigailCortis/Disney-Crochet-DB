cacb456e7be34b168193691d6f29205a83e45aad
Abigail Cortis <cortisbiggirl@gmail.com>
2025-02-13 19:36:28 +0100

    added data for issue 58

--- sql/V13__issues56-60.sql
+++ sql/V13__issues56-60.sql
@@ -1,7 +1,8 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (56, 'Frozen Snow Flower Square', ARRAY['White', 'Cyan', 'Peacock Blue', 'Periwinkle'], 2),
 (57, 'Tangled Hibiscus Petals Square', ARRAY['Hibiscus', 'Fuschia', 'White'], 1),
-(57, 'Tangled Candy Petals Square', ARRAY['Candy Pink', 'Hibiscus', 'White'], 1)
+(57, 'Tangled Candy Petals Square', ARRAY['Candy Pink', 'Hibiscus', 'White'], 1),
+(58, 'Seaweed Square', ARRAY['Lime', 'White'], 1)
 ;
 
 CREATE TYPE category_name AS ENUM('Home', 'Quick & Simple', 'Gift');
@@ -19,7 +20,10 @@ INSERT INTO small_projects (issue, project_name, difficulty, category, category_
 (56, 'Tiana Pouffe', 4, 'Home', 44),
 (57, '101 Dalmations Christmas Tree Card', 1, 'Quick & Simple', 57),
 (57, 'Anna Coin Purse', 1, 'Gift', 57),
-(57, 'Evil Queen Leaded Window Cushion', 2, 'Home', 57)
+(57, 'Evil Queen Leaded Window Cushion', 2, 'Home', 57),
+(58, 'The Emperor''s New Groove Placemats', 1, 'Quick & Simple', 58),
+(58, 'Frozen Snowflake Cushion', 2, 'Gift', 58),
+(58, 'Cars Chequered Flag Rug', 1, 'Home', 58)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -27,5 +31,8 @@ INSERT INTO skills (issue, skill) VALUES
 (56, 'Surface Bobbles'),
 (56, 'Tubular Crochet 2'),
 (57, 'Quick Block Mesh'),
-(57, 'Filet Crochet 6')
+(57, 'Filet Crochet 6'),
+(58, 'Puff Cluster Trellis'),
+(58, 'Puff Rows'),
+(58, 'Chevron Patterns 2')
 ;
