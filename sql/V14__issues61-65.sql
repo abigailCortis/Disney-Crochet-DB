15f8f2759386c1d8d3100778e02b13b69604a60b
Abigail Cortis <cortisbiggirl@gmail.com>
2025-02-25 21:07:00 +0100

    added data for issue 61

---
+++ sql/V14__issues61-65.sql
@@ -0,0 +1,14 @@
+INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
+(61, 'Snow White Forest Flower Square', ARRAY['Vanilla', 'Sunflower', 'Sky Blue', 'Royal Blue', 'Bright Red'], 2)
+;
+
+INSERT INTO small_projects (issue, project_name, difficulty, category, category_index) VALUES
+(61, 'Turning Red Flower Patches for Abby', 1, 'Quick & Simple', 61),
+(61, 'Mickey Mouse T-shirt Teddy', 3, 'Gift', 61),
+(61, 'A Bug''s Life Leafy Coasters', 1, 'Home', 61)
+;
+
+INSERT INTO skills (issue, skill) VALUES
+(61, 'Clamshell Stitch'),
+(61, 'Butttonholes 3')
+;
