fce4a8464b7b7035459eae4af857f93f238e1f8e
Abigail Cortis <cortisbiggirl@gmail.com>
2025-02-27 21:07:53 +0100

    added data for issue 62

--- sql/V14__issues61-65.sql
+++ sql/V14__issues61-65.sql
@@ -1,14 +1,21 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
-(61, 'Snow White Forest Flower Square', ARRAY['Vanilla', 'Sunflower', 'Sky Blue', 'Royal Blue', 'Bright Red'], 2)
+(61, 'Snow White Forest Flower Square', ARRAY['Vanilla', 'Sunflower', 'Sky Blue', 'Royal Blue', 'Bright Red'], 2),
+(62, 'Jungle Book Dancing Sun Square', ARRAY['Paprika', 'White', 'Sage', 'Lime', 'Coral', 'Brick Red'], 2)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty, category, category_index) VALUES
 (61, 'Turning Red Flower Patches for Abby', 1, 'Quick & Simple', 61),
 (61, 'Mickey Mouse T-shirt Teddy', 3, 'Gift', 61),
-(61, 'A Bug''s Life Leafy Coasters', 1, 'Home', 61)
+(61, 'A Bug''s Life Leafy Coasters', 1, 'Home', 61),
+(62, 'Brave Simple Scarf', 1, 'Quick & Simple', 62),
+(62, 'Simba Lion''s Mane Hat', 2, 'Gift', 62),
+(62, 'Cinderella Needle Book', 2, 'Home', 62)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
 (61, 'Clamshell Stitch'),
-(61, 'Butttonholes 3')
+(61, 'Butttonholes 3'),
+(62, 'Pineapple Stitch'),
+(62, 'Brickwork Stitch'),
+(62, 'Buttonholes 4')
 ;
