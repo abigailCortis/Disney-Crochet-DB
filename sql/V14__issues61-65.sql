d05d2d673f33ccc1df252d49dbcfd66d3b72e395
Abigail Cortis <cortisbiggirl@gmail.com>
2025-03-03 18:52:28 +0100

    added data for issue 63

--- sql/V14__issues61-65.sql
+++ sql/V14__issues61-65.sql
@@ -1,6 +1,7 @@
 INSERT INTO throw (issue, square_name, colours, difficulty) VALUES
 (61, 'Snow White Forest Flower Square', ARRAY['Vanilla', 'Sunflower', 'Sky Blue', 'Royal Blue', 'Bright Red'], 2),
-(62, 'Jungle Book Dancing Sun Square', ARRAY['Paprika', 'White', 'Sage', 'Lime', 'Coral', 'Brick Red'], 2)
+(62, 'Jungle Book Dancing Sun Square', ARRAY['Paprika', 'White', 'Sage', 'Lime', 'Coral', 'Brick Red'], 2),
+(63, 'Fiding Nemo Coral Flower Square', ARRAY['Aqua Green', 'Shell Pink', 'Coral', 'Brick Red', 'Dark Teal', 'White'], 3)
 ;
 
 INSERT INTO small_projects (issue, project_name, difficulty, category, category_index) VALUES
@@ -9,7 +10,10 @@ INSERT INTO small_projects (issue, project_name, difficulty, category, category_
 (61, 'A Bug''s Life Leafy Coasters', 1, 'Home', 61),
 (62, 'Brave Simple Scarf', 1, 'Quick & Simple', 62),
 (62, 'Simba Lion''s Mane Hat', 2, 'Gift', 62),
-(62, 'Cinderella Needle Book', 2, 'Home', 62)
+(62, 'Cinderella Needle Book', 2, 'Home', 62),
+(63, 'Raya and the Last Sragon Rustic Placemats', 1, 'Quick & Simple', 63),
+(63, 'Aladdin Margic Carpet Key Ring', 1, 'Gift', 63),
+(63, 'Cars Facecloth & Hand Towel', 3, 'Home', 63)
 ;
 
 INSERT INTO skills (issue, skill) VALUES
@@ -17,5 +21,8 @@ INSERT INTO skills (issue, skill) VALUES
 (61, 'Butttonholes 3'),
 (62, 'Pineapple Stitch'),
 (62, 'Brickwork Stitch'),
-(62, 'Buttonholes 4')
+(62, 'Buttonholes 4'),
+(63, 'Peacock Stitch'),
+(63, 'Granny Circle Motif'),
+(63, 'Woven Crochet')
 ;
